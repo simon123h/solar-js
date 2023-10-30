fb86375cb981cc84cb44baf8904dc2dc22470cfb
simon123h <simon123h@users.noreply.github.com>
2023-10-30 19:34:47 +0100

    Async visualization

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@ function add_planet(p) {
     if (!p.name.startsWith("Asteroid"))
         div.innerHTML = `<span>${p.name}</span>`;
     canvas.appendChild(div);
+    p["element"] = div;
 }
 
 
@@ -57,7 +58,7 @@ function initial_condition() {
 
 async function update_positions() {
     var scale = physics.length_scale;
-    for (var planet of solar_system) {
+    for await (var planet of solar_system) {
         var p_elmnt = document.getElementById(planet.name);
         p_elmnt.style.left = (planet.x / scale) + "px";
         p_elmnt.style.top = (planet.y / scale) + "px";
@@ -66,40 +67,18 @@ async function update_positions() {
 
 function run_simulation() {
     var n = 0;
-    var perf = { "forces": 0, "integration": 0, "positions": 0, "trace": 0 };
     var simulation_intvl = setInterval(function () {
-        var A = performance.now();
         n += 1;
-        // for (var i = 0; i < physics.substeps; i++) {
-        // update the forces
-        update_forces();
-        var A2 = performance.now();
-        // do integration step
-        integration_step();
-        // }
-        var B = performance.now();
+        // do integration step(s)
+
+        for (var i = 0; i < physics.substeps; i++) {
+            integration_step();
+        }
         // update visualization
         update_positions();
-        var C = performance.now();
         // manage trace
         if (n % 5 == 0)
             manage_trace();
-        var D = performance.now();
-        perf.forces += A2 - A;
-        perf.integration += B - A2;
-        perf.positions += C - B;
-        perf.trace += D - C;
-        if (n % 200 == 0) {
-            console.log("Performance:")
-            console.log(" - Forces     :", perf.forces / n);
-            console.log(" - Integration:", perf.integration / n);
-            console.log(" - Positions  :", perf.positions / n);
-            console.log(" - Trace      :", perf.trace / n);
-            // console.log(" - Integration:", 100*perf.integration/total);
-            // console.log(" - Positions  :", 100*perf.positions/total);
-            // console.log(" - Trace      :", 100*perf.trace/total);
-            perf = { "forces": 0, "integration": 0, "positions": 0, "trace": 0 };
-        }
     }, 20);
     return simulation_intvl;
 }
@@ -134,6 +113,7 @@ function update_forces() {
 
 function integration_step() {
     var dt = physics.dt;
+    update_forces();
     for (var p of solar_system) {
         // store old values
         p["old_x"] = p.x;
@@ -162,7 +142,7 @@ async function manage_trace() {
     var canvas = document.getElementById("canvas");
     var time = physics.time;
     var max_age = physics.trace_age;
-    for (var planet of solar_system) {
+    for await (var planet of solar_system) {
         if (planet.name.startsWith("Asteroid"))
             continue;
         var pel = document.getElementById(planet.name);
@@ -181,3 +161,14 @@ async function manage_trace() {
             $(this).remove();
     });
 }
+
+function solar_dict() {
+    var result = {};
+    for (var planet of solar_system) {
+        result[planet.name] = planet;
+    }
+    return result;
+}
+
+// convert to associative array
+ss = solar_dict()
--- js/solar_system.js
+++ js/solar_system.js
@@ -2,11 +2,11 @@
 var physics = {
     "G": 6.674e-11, // Graviational constant
     "length_scale": 1e9, // length scale
-    "dt": 60 * 60 * 24, // time step size
+    "dt": 60 * 60 * 24/12, // time step size
     "trace_age": 60 * 60 * 24 * 400,
     "time": 0,
     "substeps": 1,
-    "n_asteroids": 100,
+    "n_asteroids": 200,
 }
 
 var solar_system = [
@@ -14,7 +14,7 @@ var solar_system = [
         "name": "Sonne",
         "color": "#ff0",
         "mass": 1.988e30,
-        "radius": 12,
+        "radius": 20,
         "orbitRadius": 0,
     },
     // {
@@ -28,14 +28,14 @@ var solar_system = [
         "name": "Merkur",
         "color": "#999999",
         "mass": 3.3011e23,
-        "radius": 5,
+        "radius": 2,
         "orbitRadius": 5.9133e10,
     },
     {
         "name": "Venus",
         "color": "#ffff99",
         "mass": 4.8675e23,
-        "radius": 5,
+        "radius": 4,
         "orbitRadius": 1.082e11
     },
     {
@@ -49,44 +49,44 @@ var solar_system = [
         "name": "Mars",
         "color": "#ff0000",
         "mass": 6.4185e23,
-        "radius": 5,
+        "radius": 4,
         "orbitRadius": 2.289e11
     },
     {
         "name": "Jupiter",
         "color": "#ff8c00",
         "mass": 1.8986e27,
-        "radius": 5,
+        "radius": 11,
         "orbitRadius": 7.793e11
     },
-    // {
-    //     "name": "Saturn",
-    //     "color": "#ffff80",
-    //     "mass": 5.6846e26,
-    //     "radius": 5,
-    //     "orbitRadius": 1.429e12
-    // },
-    // {
-    //     "name": "Uranus",
-    //     "color": "#7fffd4",
-    //     "mass": 8.6832e25,
-    //     "radius": 5,
-    //     "orbitRadius": 2.874e12
-    // },
-    // {
-    //     "name": "Neptun",
-    //     "color": "#4169e1",
-    //     "mass": 1.0243e26,
-    //     "radius": 5,
-    //     "orbitRadius": 4.498e12
-    // },
-    // {
-    //     "name": "Pluto",
-    //     "color": "#808080",
-    //     "mass": 1.303e22,
-    //     "radius": 5,
-    //     "orbitRadius": 6.089e12
-    // }
+    {
+        "name": "Saturn",
+        "color": "#ffff80",
+        "mass": 5.6846e26,
+        "radius": 5,
+        "orbitRadius": 1.429e12
+    },
+    {
+        "name": "Uranus",
+        "color": "#7fffd4",
+        "mass": 8.6832e25,
+        "radius": 5,
+        "orbitRadius": 2.874e12
+    },
+    {
+        "name": "Neptun",
+        "color": "#4169e1",
+        "mass": 1.0243e26,
+        "radius": 5,
+        "orbitRadius": 4.498e12
+    },
+    {
+        "name": "Pluto",
+        "color": "#808080",
+        "mass": 1.303e22,
+        "radius": 5,
+        "orbitRadius": 6.089e12
+    }
 ];
 
 // add the asteroid belt
@@ -96,7 +96,6 @@ for (var n = 0; n < physics.n_asteroids; n++) {
         "color": "#AAA",
         "mass": 1e15,
         "radius": 1,
-        "orbitRadius": 4e11 * (1 + 0.1 * Math.random()),
+        "orbitRadius": 4e11 * (1 + 0.05 * Math.random()),
     });
 }
-
