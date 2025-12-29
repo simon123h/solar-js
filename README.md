d0bf54f8e72bb0f315ca5c6845e2ba331b29c417
simon123h <simon123h@users.noreply.github.com>
2025-12-29 15:52:40 +0100

    build: add npm and vite

--- README.md
+++ README.md
@@ -5,3 +5,8 @@
 ![Galaxy Collision Simulation](img/galaxy-collision.gif)
 
 ![N-Body Problem Simulation](img/n-body-universe.gif)
+
+## First steps
+
+1. Launch a web server for the game using the command `npm run dev` in a shell
+2. Open the game under <https://localhost:5173>
---
+++ vite.config.js
@@ -0,0 +1,10 @@
+export default {
+  root: ".",
+  build: {
+    outDir: "dist", // the directory where the built files will go
+    emptyOutDir: true, // clear the output directory before building
+  },
+  server: {
+    open: true, // open the browser automatically when the server starts
+  },
+};
