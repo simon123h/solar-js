874395a2ac1245b5a192eb7f3f735c5667e2dbb2
simon123h <simon123h@users.noreply.github.com>
2025-12-29 16:27:59 +0100

    ci: conditionally build the project with a different base path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
-export default {
-  root: ".",
-  build: {
-    outDir: "dist", // the directory where the built files will go
-    emptyOutDir: true, // clear the output directory before building
-  },
-  server: {
-    open: true, // open the browser automatically when the server starts
-  },
-};
+import { defineConfig } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const basePath = process.env.VITE_BASE_PATH;
+
+  return {
+    root: ".",
+    base: basePath ? `/${basePath}/` : "/",
+    build: {
+      outDir: "dist", // the directory where the built files will go
+      emptyOutDir: true, // clear the output directory before building
+    },
+    server: {
+      open: true, // open the browser automatically when the server starts
+    },
+  };
+});
